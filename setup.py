13134142dcb8ed80e99fd2175fa4abcdc28929fa
Marin Atanasov Nikolov <dnaeon@gmail.com>
2013-05-14 21:01:09 +0300

    Commit setup.py for building the Python module

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(name='pkglib',
+      version='0.0.1',
+      description='Python bindings to libpkg',
+      author='Marin Atanasov Nikolov',
+      author_email='dnaeon@gmail.com',
+      license='BSD',
+      ext_modules=[
+          Extension(
+              'pkglib',
+              include_dirs=['/usr/local/include'],
+              library_dirs=['/usr/local/lib'],
+              libraries=['pkg'],
+              sources=['pkglibmodule.c']
+          )
+      ]
+)
+
