506d0ebd9fcd4cd08484ade123722c0722c2210d
Marin Atanasov Nikolov <dnaeon@gmail.com>
2013-05-14 22:15:40 +0300

    Initial commit of the high-level library for pkg(8) Python module

---
+++ pkg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pkglib
+
+class PkgDb(object):
+	def __init__(self, remotedb=False):
+                self.__db = pkglib.db_open(remotedb)
+
+        def query(self, pkgname):
+                return PkgIt(self.__db, pkgname)
+
+class PkgIt(object):
+        def __init__(self, db, pkgname):
+                self.__it = pkglib.db_query(db, pkgname)
+
+        def __iter__(self):
+                return self
+
+        def next(self):
+                result = pkglib.db_list(self.__it)
+
+                if not result:
+                        raise StopIteration
+                else:
+                        return result
+
+
