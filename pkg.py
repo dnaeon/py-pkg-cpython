8b7d64d7d59c5ef57061eb9db0506d03b4b6ad96
Marin Atanasov Nikolov <dnaeon@gmail.com>
2013-05-16 15:56:03 +0300

    Implement a 'Pkg' class type along with methods for getting pkg attributes

--- pkg.py
+++ pkg.py
@@ -1,30 +1,44 @@
-#!/usr/bin/env python
-
 import pkglib
 
 class PkgDb(object):
 	def __init__(self, remotedb=False):
-                self.__db = pkglib.db_open(remotedb)
+                self._db = pkglib.db_open(remotedb)
 
         def close(self):
-                pkglib.db_close(self.__db)
+                pkglib.db_close(self._db)
 
         def query(self, pkgname):
-                return PkgIt(self.__db, pkgname)
+                return PkgIt(self._db, pkgname)
 
-class PkgIt(object):
+class PkgIt(PkgDb):
         def __init__(self, db, pkgname):
-                self.__it = pkglib.db_query(db, pkgname)
+                self._it = pkglib.db_query(db, pkgname)
 
         def __iter__(self):
                 return self
 
         def next(self):
-                result = pkglib.db_list(self.__it)
+                result = pkglib.db_list(self._it)
 
                 if not result:
                         raise StopIteration
                 else:
-                        return result
+                        return Pkg(result)
+
+class Pkg(object):
+        def __init__(self, pkg):
+                self._pkg = pkg
+
+        def __str__(self):
+                return '%s-%s: %s' % (self.name(), self.version(), self.comment())
+
+        def name(self):
+                return pkglib.pkg_get_name(self._pkg)
+
+        def version(self):
+                return pkglib.pkg_get_version(self._pkg)
+
+        def comment(self):
+                return pkglib.pkg_get_comment(self._pkg)
 
 
