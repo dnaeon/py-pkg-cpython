b454af1baa8da8c168fa368c40079c4cddc41e47
Marin Atanasov Nikolov <dnaeon@gmail.com>
2013-05-21 07:48:33 +0300

    Rename method name to query_info()

--- pkg.py
+++ pkg.py
@@ -10,7 +10,7 @@ class PkgDb(object):
         def close(self):
                 pkglib.db_close(self._db)
 
-        def query(self, pkgname=None, match_regex=False):
+        def query_info(self, pkgname=None, match_regex=False):
                 return PkgIter(self._db, pkgname, match_regex)
 
         def query_install(self, pkgname, match_regex=False):
